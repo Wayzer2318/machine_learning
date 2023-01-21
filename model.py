46a6da2f877d58d6fd5463891b674c5e6ee85116
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-21 04:47:15 +0100

    creating a dataset generator

---
+++ model.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def get_dataset():
+    # method to get a dataset
+
+    # number of row per class
+    row_per_class = 5
+
+    #generate rows 
+    print (np.random.randn(row_per_class, 2))
+
+
+
+
+
+if __name__ == "__main__":
+    get_dataset()
+    pass
+
