021f4975c168c759c76e2fe02dd2cdcc12f811a8
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-21 05:14:51 +0100

    init model variable w/b

--- model.py
+++ model.py
@@ -1,20 +1,35 @@
 import numpy as np
 
+def init_variables():
+    """
+        init model variables (weight - bias)
+    """
+    weights = np.random.normal(size=2)
+    bias = 0 
+    
+
+    return weights, bias
+
 
 def get_dataset():
-    # method to get a dataset
-
+    """
+        method to get a dataset
+    """
     # number of row per class
     row_per_class = 5
 
     #generate rows 
-    print (np.random.randn(row_per_class, 2))
+    sick = np.random.randn(row_per_class, 2) + np.array([-2, -2])
+    healthy = np.random.randn(row_per_class, 2) + np.array([2, 2])
+    
+    features = np.vstack([sick, healthy])
+    targets = np.concatenate((np.zeros(row_per_class), np.zeros(row_per_class) + 1))
 
-
-
+    return features, targets 
 
 
 if __name__ == "__main__":
-    get_dataset()
+    features, targets = get_dataset()
+    init_variables()
     pass
 
