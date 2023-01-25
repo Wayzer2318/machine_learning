ccd5c8bffd209c9de6b95a32fa5ea6ca256133bd
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-25 04:08:21 +0100

    training model

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 def init_variables():
     """
         init model variables (weight - bias)
@@ -15,7 +17,7 @@ def get_dataset():
         method to get a dataset
     """
     # number of row per class
-    row_per_class = 5
+    row_per_class = 100
 
     #generate rows 
     sick = np.random.randn(row_per_class, 2) + np.array([-2, -2])
@@ -39,16 +41,58 @@ def activation(z):
     """
     return 1 / (1 + np.exp(-z))
 
+def d_activation(z):
+    return activation(z) * (1 - activation(z))
 
+def predict(features, weights, bias):
+    """
+    """
+    z = pre_activation(features, weights, bias)
+    y = activation(z)
+    return np.round(y)
 
+def cost(predictions, targets):
+    """
+    """
+    return np.mean((predictions - targets)**2)
+
+def train(features, targets, weights, bias):
+    """
+    """
+    epoch = 100
+    learning_r = 0.1
+    #print current accurency
+    predictions = predict(features, weights, bias)
+    print("Accurency: ", np.mean(predictions == targets))
+    # Plot points
+    plt.scatter(features[:, 0], features[:, 1], s=40, c=targets, cmap=plt.cm.Spectral)
+    plt.show()
+    
+    for epoch in range(epoch):
+       # Compute and display the cost every 10 epoch
+        if epoch % 10 == 0:
+            predictions = activation(pre_activation(features, weights, bias))
+            print("Current cost = %s" % cost(predictions, targets))
+        # Init gragients
+        weights_gradients = np.zeros(weights.shape)
+        bias_gradient = 0.
+        # Go through each row
+        for feature, target in zip(features, targets):
+            # Compute prediction
+            z = pre_activation(feature, weights, bias)
+            y = activation(z)
+            # Update gradients
+            weights_gradients += (y - target) * d_activation(z) * feature
+            bias_gradient += (y - target) * d_activation(z)
+        # Update variables
+        weights = weights - (learning_r * weights_gradients)
+        bias = bias - (learning_r * bias_gradient)
+    #print current accurency
+    predictions = predict(features, weights, bias)
+    print("Accurency: ", np.mean(predictions == targets))
 if __name__ == "__main__":
     #rows
     features, targets = get_dataset()
     #variable
     weights, bias = init_variables()
-    # calculate the pre-activation
-    z = pre_activation(features, weights, bias)
-    a = activation(z)
-    print(a)
-    pass
-
+    train(features, targets, weights, bias)
